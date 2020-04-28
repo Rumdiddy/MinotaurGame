dd7b81b2abb459e2c4872d752ab66811dbdbae2c
Ajay Ananthakrishnan <ajayananth@jhu.edu>
2020-04-27 21:45:01 -0400

    added goal

---
+++ starter-code/goal.cpp
@@ -0,0 +1,20 @@
+#include "goal.h"
+
+Goal::Goal() {
+}
+
+Goal::~Goal() {
+}
+
+bool Goal::isGoal() {
+  return true; 
+}
+
+//TODO: Move result.
+
+std::string Goal::getGlyph() {
+  return "<"; 
+}
+
+
+
